206a9d0472b4b6955aa95fdb8d0608ad5bc6a118
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2020-12-21 16:25:47 +0000

    Remove references to @conn in error templates

--- apps/shoutouts_web/test/testimonials_web/views/error_view_test.exs
+++ apps/shoutouts_web/test/testimonials_web/views/error_view_test.exs
@@ -4,11 +4,15 @@ defmodule ShoutoutsWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  test "renders 403.html" do
+    assert render_to_string(ShoutoutsWeb.ErrorView, "403.html", []) =~ "Unauthorized"
+  end
+
   test "renders 404.html" do
-    assert render_to_string(ShoutoutsWeb.ErrorView, "404.html", []) =~ "does not exist"
+    assert render_to_string(ShoutoutsWeb.ErrorView, "404.html", []) =~ "Not found"
   end
 
   test "renders 500.html" do
-    assert render_to_string(ShoutoutsWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(ShoutoutsWeb.ErrorView, "500.html", []) =~ "Oops, something's wrong"
   end
 end
