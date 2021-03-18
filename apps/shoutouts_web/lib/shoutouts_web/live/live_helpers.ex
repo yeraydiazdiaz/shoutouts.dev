7e127af53532f4eccc76625f18a69e8d2d50f9bf
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2021-03-18 08:43:26 +0000

    Rename unused variable

--- apps/shoutouts_web/lib/shoutouts_web/live/live_helpers.ex
+++ apps/shoutouts_web/lib/shoutouts_web/live/live_helpers.ex
@@ -15,7 +15,7 @@ defmodule ShoutoutsWeb.LiveHelpers do
         project: @project,
         return_to: Routes.project_index_path(@socket, :index) %>
   """
-  def live_modal(socket, component, opts) do
+  def live_modal(_socket, component, opts) do
     path = Keyword.fetch!(opts, :return_to)
     modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
     live_component(socket, ShoutoutsWeb.ModalComponent, modal_opts)
