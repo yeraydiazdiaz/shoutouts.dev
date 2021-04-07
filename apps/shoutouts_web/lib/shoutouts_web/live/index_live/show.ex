07bde6cf21fcbfb2972ffe18825a504388542514
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2021-04-07 14:53:19 +0100

    Render a fake shoutout if none exist

--- apps/shoutouts_web/lib/shoutouts_web/live/index_live/show.ex
+++ apps/shoutouts_web/lib/shoutouts_web/live/index_live/show.ex
@@ -1,6 +1,7 @@
 defmodule ShoutoutsWeb.IndexLive.Show do
   use ShoutoutsWeb, :live_view
   alias Shoutouts.Accounts
+  alias Shoutouts.Projects
   alias Shoutouts.Shoutouts
 
   require Logger
@@ -18,7 +19,10 @@ defmodule ShoutoutsWeb.IndexLive.Show do
         do: Accounts.get_user!(current_user_id),
         else: nil
 
-    shoutouts = Shoutouts.shoutouts_for_top_projects()
+    shoutouts = case Shoutouts.shoutouts_for_top_projects() do
+      [] -> [default_shoutout()]
+      stp -> stp
+    end
 
     if connected?(socket), do: Process.send_after(self(), :carrousel_switch, @carrousel_timeout)
 
@@ -34,5 +38,19 @@ defmodule ShoutoutsWeb.IndexLive.Show do
     Process.send_after(self(), :carrousel_switch, @carrousel_timeout)
     next_idx = rem((socket.assigns[:shoutout_idx] + 1), length(socket.assigns[:shoutouts]))
     {:noreply, assign(socket, :shoutout_idx, next_idx)}
+  end
+
+  defp default_shoutout() do
+    %Shoutouts.Shoutout{
+      text: "Your project is amazing!\nThanks for everything you do!",
+      user: %Accounts.User{
+        name: "Amy Grateful",
+        signature: "Prolific Open Source contributor"
+      },
+      project: %Projects.Project{
+        owner: "yeraydiazdiaz",
+        name: "shoutouts.dev"
+      }
+    }
   end
 end
