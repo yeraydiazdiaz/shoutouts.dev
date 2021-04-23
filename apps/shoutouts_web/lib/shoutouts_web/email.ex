aeb51c9f515565cd668bd1b4cf30b39b8b95ad0e
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2021-04-23 14:44:37 +0100

    Shoutouts digest email logic

--- apps/shoutouts_web/lib/shoutouts_web/email.ex
+++ apps/shoutouts_web/lib/shoutouts_web/email.ex
@@ -1,18 +1,27 @@
 defmodule ShoutoutsWeb.Email do
   use Bamboo.Phoenix, view: ShoutoutsWeb.EmailView
-  alias Shoutouts.Accounts
 
   @default_sender "no-reply@shoutouts.dev"
 
-  def shoutout_digest(%Shoutouts.Accounts.User{} = user) do
+  def shoutout_digest() do
+    Shoutouts.Shoutouts.unnotified_shoutouts()
+    |> Enum.group_by(&(&1.project.user))
+    |> Enum.map(fn {project, shoutouts} -> shoutout_digest_for_user(project, shoutouts) end)
+  end
+
+  def shoutout_digest_for_user(%Shoutouts.Accounts.User{} = user, shoutouts) do
+    user_names = (for s <- shoutouts, into: MapSet.new(), do: s.user.name) |> MapSet.to_list
+    project_owner_names = shoutouts
+      |> Enum.map(fn s -> {s.project.owner, s.project.name} end)
     new_email(
       to: user.email,
       from: @default_sender,
       subject: "New shoutouts for your projects"
     )
     |> put_html_layout({ShoutoutsWeb.LayoutView, "email.html"})
-    |> assign(:first_name, Accounts.first_name(user))
-    |> assign(:user, user)
+    |> assign(:first_name, Shoutouts.Accounts.first_name(user))
+    |> assign(:user_names, user_names)
+    |> assign(:project_owner_names, project_owner_names)
     |> render(:shoutouts_digest)
   end
 end
--- apps/shoutouts_web/test/shoutouts_web/views/email_view_test.exs
+++ apps/shoutouts_web/test/shoutouts_web/views/email_view_test.exs
@@ -3,14 +3,27 @@ defmodule ShoutoutsWeb.EmailTest do
 
   alias Shoutouts.Factory
 
-  describe "Email" do
-    test "shoutout_digest" do
+  describe "shoutout_digest" do
+    test "single project, > 2 shoutouts" do
       user = Factory.insert(:user, name: "Yeray Diaz Diaz")
-      email = ShoutoutsWeb.Email.shoutout_digest(user)
+      project = Factory.insert(:project, user: user)
+      shoutouts = for _ <- 1..3, do: Factory.insert(:shoutout, project: project)
+      [email] = ShoutoutsWeb.Email.shoutout_digest()
+
       assert email.to == user.email
       assert email.from == "no-reply@shoutouts.dev"
       assert email.subject == "New shoutouts for your projects"
-      assert email.text_body =~ "Hi Yeray"
+
+      assert email.html_body =~ Routes.project_show_url(ShoutoutsWeb.Endpoint, :show, project.owner, project.name)
+      combinations = for x <- shoutouts,
+        y <- shoutouts, x.user.name != y.user.name,
+        do: "#{x.user.name}, #{y.user.name}"
+      assert Enum.any?(combinations, fn combination -> email.html_body =~ combination end)
+      assert email.html_body =~ "and 1 other people"
+
+      assert email.text_body =~ Routes.project_show_url(ShoutoutsWeb.Endpoint, :show, project.owner, project.name)
+      assert Enum.any?(combinations, fn combination -> email.text_body =~ combination end)
+      assert email.text_body =~ "and 1 other people"
     end
   end
 end
