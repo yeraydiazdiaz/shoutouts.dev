076f8802621b86f0e2b60b18aad54a7f4d8c72ca
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2021-09-19 16:51:00 +0100

    21.3.2

--- apps/shoutouts/mix.exs
+++ apps/shoutouts/mix.exs
@@ -4,7 +4,7 @@ defmodule Shoutouts.MixProject do
   def project do
     [
       app: :shoutouts,
-      version: "21.3.1",
+      version: "21.3.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/shoutouts_web/mix.exs
+++ apps/shoutouts_web/mix.exs
@@ -4,7 +4,7 @@ defmodule ShoutoutsWeb.MixProject do
   def project do
     [
       app: :shoutouts_web,
-      version: "21.3.1",
+      version: "21.3.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
